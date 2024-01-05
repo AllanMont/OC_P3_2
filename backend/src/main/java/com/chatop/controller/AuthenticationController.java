4afbec1ad81df605264d937c37c878f0c7ed4d6f
AllanMont <allan.mont34@gmail.com>
2024-01-05 06:13:44 +0100

    add login function

--- backend/src/main/java/com/chatop/controller/AuthenticationController.java
+++ backend/src/main/java/com/chatop/controller/AuthenticationController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.dto.LoginRequest;
 import com.chatop.dto.RegisterRequest;
 import com.chatop.model.Authentication;
 import com.chatop.service.AuthenticationService;
-
-import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,9 +17,13 @@ public class AuthenticationController {
     
     private AuthenticationService AuthenticationService;
 
-    @Operation(summary = "auth register")
     @PostMapping("/register")
     public ResponseEntity<Authentication> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(AuthenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Authentication> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(AuthenticationService.login(request));
     }
 }
