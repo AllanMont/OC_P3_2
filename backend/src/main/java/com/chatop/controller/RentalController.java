c6c65324836224f92e0e085750e2ebc4b2a63137
AllanMont <allan.mont34@gmail.com>
2024-01-05 01:56:00 +0100

    feat/finish api to create rentals

--- backend/src/main/java/com/chatop/controller/RentalController.java
+++ backend/src/main/java/com/chatop/controller/RentalController.java
@@ -5,17 +5,21 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.dto.RentalDto;
 import com.chatop.model.Rental;
 import com.chatop.service.RentalService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/rentals")
@@ -44,9 +48,20 @@ public class RentalController {
 		return ResponseEntity.notFound().build();
 	}
 
-	@PostMapping
-	public ResponseEntity<String> newRental(@RequestBody Rental newRental) {		
-	    Rental createdRental = rentalService.createRental(newRental);
+	@PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+	public ResponseEntity<String> newRental( HttpServletRequest request,
+      @ModelAttribute("rentals") RentalDto rentalsDto) {
+
+		String urlPicture = rentalService.uploadPicture(rentalsDto.getPicture());
+		if(urlPicture == null) { return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Échec de l'upload de l'image."); }		
+	    
+		Rental newRental = new Rental();
+		newRental.setName(rentalsDto.getName());
+		newRental.setPrice(rentalsDto.getPrice());
+		newRental.setSurface(rentalsDto.getSurface());
+		newRental.setPicture(urlPicture);
+		newRental.setDescription(rentalsDto.getDescription());
+		Rental createdRental = rentalService.createRental(newRental);
 
 	    if (createdRental != null) {
 	        return ResponseEntity.ok("Location créée avec succès !");
--- backend/src/main/java/com/chatop/service/RentalService.java
+++ backend/src/main/java/com/chatop/service/RentalService.java
@@ -1,5 +1,11 @@
 package com.chatop.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +13,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.chatop.model.Rental;
 import com.chatop.repository.RentalRepository;
@@ -26,7 +34,31 @@ public class RentalService {
   }
 
   public Rental createRental(Rental Rental) {
+      // Save Rental with increment id
+      int newId = rentalRepository.findAll().size() + 1;
+      Rental.setOwner_id(newId);
+
       Rental savedRental = rentalRepository.save(Rental);
       return savedRental;
   }
+
+ public String uploadPicture(MultipartFile file) {
+    String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+    Path uploadPath = Paths.get("uploads");
+    if (!Files.exists(uploadPath)) {
+      try {
+        Files.createDirectories(uploadPath);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    try (InputStream inputStream = file.getInputStream()) {
+      Path filePath = uploadPath.resolve(fileName);
+      Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return uploadPath + "/" + fileName;
+  }
+
 }
